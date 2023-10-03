f74bff8a23277e0260b41b587f6e14c44c809a71
Júlio Soares <juliocsoaresa1@gmail.com>
2023-10-03 13:09:27 -0300

    ex003

---
+++ exercises/ex003.py
@@ -0,0 +1,9 @@
+# Create a program that read 2 numbers and displays the sum between them
+# Difficult: Easy
+
+n1 = float(input('First number: '))
+n2 = float(input('Second number: '))
+
+sum = n1 + n2
+
+print(f'{n1} + {n2} = {sum}')
