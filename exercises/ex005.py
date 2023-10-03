b38f56f1b449ed7c1ded5966303c61737755f02d
Júlio Soares <juliocsoaresa1@gmail.com>
2023-10-03 14:33:54 -0300

    ex005
    ake a program that reads an integer and displays its successor and predecessor on the screen

---
+++ exercises/ex005.py
@@ -0,0 +1,8 @@
+# Make a program that reads an integer and displays its successor and predecessor on the screen
+# Difficult: Easy
+
+n = int(input('Write a number: '))
+predecessor = n - 1
+sucessor = n + 1
+
+print(f'Analyzing the value {n}, the predecessor is {predecessor} and the sucessor is {sucessor}')
