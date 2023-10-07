2f702399f97c09d9c788a6c9f1ea4c6a576a65ad
Júlio Soares <juliocsoaresa1@gmail.com>
2023-10-07 14:08:46 -0300

    ex014
    Make a program that converts °C to °F

---
+++ exercises/ex014.py
@@ -0,0 +1,7 @@
+# Make a program that converts °C to °F
+# Difficult: Easy
+
+celsius = float(input('Informe a temperatura em °C: '))
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f'A temperatura de {celsius}°C corresponde a {fahrenheit}°F!')
