cb557643867df86227e634d6c5fcb9ae440a54fd
Júlio Soares <juliocsoaresa1@gmail.com>
2023-10-08 21:28:55 -0300

    ex016
    Make a program that reads any real number from the keyboard and shows on the screen its entire portion.

---
+++ exercises/ex016.py
@@ -0,0 +1,7 @@
+# Make a program that reads any real number from the keyboard and shows on the screen its entire portion.
+# Difficult: Easy
+
+n = float(input('Type a number: '))
+integer = int(n)
+
+print(f'The integer part of {n} is {integer}')
